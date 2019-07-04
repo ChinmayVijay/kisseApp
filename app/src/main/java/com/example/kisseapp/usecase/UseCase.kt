84cd54df315da9524c7f44b34712336b2baaedf1
Chinmay <ichinmay08@gmail.com>
2019-07-04 11:14:36 +0530

    Added UseCase Layer

---
+++ app/src/main/java/com/example/kisseapp/usecase/UseCase.kt
@@ -0,0 +1,37 @@
+package com.example.kisseapp.usecase
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+suspend operator <R> fun UseCase<Unit,R>.invoke():LiveData<Result<R>> = this(Unit)
+
+suspend operator <R> fun UseCase<Unit,R>.invoke(result: MutableLiveData<Result<R>>) = this(Unit,Unit)
+
+abstract class UseCase<in P, R> {
+    abstract suspend fun execute(request:P): R
+
+    suspend fun executeSync(request: P):Result<R>{
+        return try{
+            Result.success(execute(request))
+        } catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+
+    suspend operator fun invoke(request:P, result:MutableLiveData<R>){
+        try{
+            execute(request).let { useCaseResult ->
+                result.postValue(Result.success(useCaseResult))
+            }
+        } catch (e: Exception){
+            result.postValue(Result.failure(e))
+        }
+    }
+
+    suspend operator fun invoke(request: P):LiveData<Result<R>>{
+        val liveData = MutableLiveData<Result<R>>()
+        this(request,liveData)
+        return liveData
+    }
+}
+
